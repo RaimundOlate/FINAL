ef889f274f77617b101b4d49f12d2f4697826276
Raimundo Olate <raimundolatetoro@hotmail.com>
2023-07-17 11:42:49 -0400

    agrega modelo a django

--- vehiculo/admin.py
+++ vehiculo/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Auto
+
+
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'marca', 'modelo', 'serial_carroceria',
+                    'serial_motor', 'categoria', 'precio')
+    ordering = ('marca', 'modelo')
+
+
+admin.site.register(Auto, AutoAdmin)
